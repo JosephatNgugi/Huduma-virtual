6cc41cdf312c55a649838bdb4487240026098e68
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-03-21 17:19:18 +0300

    Fix the navbars and add a footer date updater function

--- js/guides.js
+++ js/guides.js
@@ -1,1 +1,11 @@
 
+
+
+
+
+
+
+            // AUTOMATIC YEAR UPDATE 
+let date = new Date().getFullYear();
+let dateTag = document.getElementById("year");
+dateTag.innerHTML = `Copyright(C) <br> ${date}`;
