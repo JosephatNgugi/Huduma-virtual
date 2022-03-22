037a6e9aa56f4673bf52f0c2f9b9b7299634e4a8
Rachel Kiarie <rachelnnk94@gmail.com>
2022-03-22 19:19:06 +0300

    Add js code for the newsletter form.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// AUTOMATIC YEAR UPDATE 
+function Newsletter (fullname, email) {
+  this.fullName = fullname;
+  this.emailAddress = email;
+}
+$(document).ready(function() {
+  $("#newsletter-form").submit(function(event){
+    event.preventDefault();
+    let inputtedName = $("#fullname").val();
+    let inputtedEmail = $("#email").val();
+    let newNewsletter = Newsletter(inputtedName, inputtedName);
+    alert("Hello " + fullName + ", thank you for signing up for our newsletter.")
+
+
+  });
+});
