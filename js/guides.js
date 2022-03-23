daf4eacf05d92860bcfeeaf6785aa39cfb2ad93f
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-03-23 23:45:27 +0300

    add an alert function to the newsletter

--- js/guides.js
+++ js/guides.js
@@ -41,7 +41,19 @@ $(function(){
     // $("nav").onscroll
 });
 
-
+// FORM NEWSLETTER
+function Newsletter(email) {
+  this.emailAddress = email;
+}
+$(document).ready(function () {
+  $("#newsletter-form").submit(function (event) {
+    event.preventDefault();
+    let inputtedEmail = $("#email").val();
+    let newNewsletter = Newsletter(inputtedEmail);
+    let message = "Thank you for subscribing!";
+    alert(message);
+  });
+});
 
 
 
