e54461b56e4c10c790bc12b62231138f4ce18fb8
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-03-23 19:50:30 +0300

    Updates on Nav and footer. Fix the navbar to the top of the page. Add a footer and style it. change hover effect color of nav links.

--- js/guides.js
+++ js/guides.js
@@ -1,11 +1,13 @@
 $(function(){
     $("#kra").click(function(){
         $(".main").hide(1000);
+        $("#mainShow").show(1200);
         // $("#tutorials").toggle(1000);
         $("#tutorials").show(1000);
     });
     $("#mainShow").click(function () {
         $("#tutorials").hide(1000);
+        $("#mainShow").hide(100);
         // $(".main").toggle(1000);
         $(".main").show(1000);
     });
@@ -13,17 +15,30 @@ $(function(){
 
     $(".pinAppl").click(function () {
         $("#kraGuides").hide(1000);
+        $("#mainShow").hide(100);
         // $(".kraPinAppl").toggle(1000);
+        $("#showKraGuides").show(1200);
         $(".kraPinAppl").show(1000);
     });
     $("#showKraGuides").click(function () {
-      $(".kraPinAppl").hide(1000);
+        $(".kraPinAppl").hide(1000);
+        $("#showKraGuides").hide();
     //   $("#kraGuides").toggle(1000);
-      $("#kraGuides").show(1000);
+        $("#mainShow").show(100);
+        $("#kraGuides").show(1000);
     });
         // toggle tutorial and guides 
 
-    
+    // $(".comingSoon").click(function () {
+    //     $(".main").hide(1000);
+    //   // $(".kraPinAppl").toggle(1000);
+    //     $("#more").show(1000);
+    //     $("#mainShow").show(100);
+
+    // });
+
+
+    // $("nav").onscroll
 });
 
 
@@ -35,4 +50,4 @@ $(function(){
             // AUTOMATIC YEAR UPDATE 
 let date = new Date().getFullYear();
 let dateTag = document.getElementById("year");
-dateTag.innerHTML = `Copyright(C) <br> ${date}`;
+dateTag.innerHTML = `Copyright © ${date}`;
